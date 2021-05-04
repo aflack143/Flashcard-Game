0e05b8eff943c3bc485a292f2265deb51ef2d167
Andrea Clark <andrea_clark@glic.com>
2021-05-04 09:57:55 -0500

    working flips

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-const playingTable = document.querySelector(".playingTable");
-let eachCard = playingTable.forEach;
-playingTable.addEventListener("click", function() {
-    playingTable.classList.toggle("flip");
-});
+// const playingTable = document.querySelector(".playingTable");
+
+// playingTable.addEventListener("click", function() {
+//     playingTable.classList.toggle("flip");
+// });
+
+
+const eachCard = document.querySelectorAll(".flipCard");
+eachCard.forEach(card => card.addEventListener("click", flipCard));
+function flipCard() {
+    this.classList.toggle("flip"); 
+}
